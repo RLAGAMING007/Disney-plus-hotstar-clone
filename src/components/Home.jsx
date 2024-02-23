d3cd8dda33e31350433c3af3a2c608a3d8927331
RLAGAMING007 <k.l.kumar006@gmail.com>
2024-02-23 13:58:37 +0530

    ADD: viewers

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import ImageSlider from "./ImageSlider";
+import Viewers from "./Viewers";
 
 const Home = () => {
   return (
     <Container>
       <ImageSlider />
+      <Viewers />
     </Container>
   );
 };
 
+export default Home;
+
 const Container = styled.main`
+  overflow-x: hidden;
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
@@ -26,5 +31,3 @@ const Container = styled.main`
     z-index: -1;
   }
 `;
-
-export default Home;
--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,13 +8,12 @@ import "slick-carousel/slick/slick-theme.css";
 const ImageSlider = () => {
   let settings = {
     dots: true,
-    infinity: true,
+    infinite: true,
     speed: 500,
-    sliderToShow: 1,
-    sliderToScroll: 1,
-    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoPlay: true,
   };
-
   return (
     <Carousel {...settings}>
       <Wrap>
@@ -26,20 +26,21 @@ const ImageSlider = () => {
   );
 };
 
+export default ImageSlider;
+
 const Carousel = styled(Slider)`
   margin-top: 20px;
 
   ul li button {
     &:before {
       font-size: 10px;
-      color: rgb(150 158, 171);
+      color: rgb(150, 158, 171);
     }
   }
 
   li.slick-active button:before {
     color: white;
   }
-
   .slick-list {
     overflow: visible;
   }
@@ -47,19 +48,20 @@ const Carousel = styled(Slider)`
     z-index: 1;
   }
 `;
+
 const Wrap = styled.div`
+  cursor: pointer;
   img {
-    border: 4px solid transparent;
-    border-radius: 4px;
     width: 100%;
     height: 100%;
+    border-radius: 4px;
+    border: 4px solid transparent;
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
       rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-    transition: 300ms;
+    transition-duration: 300ms;
+
     &:hover {
       border: 4px solid rgba(249, 249, 249, 0.8);
     }
   }
 `;
-
-export default ImageSlider;
